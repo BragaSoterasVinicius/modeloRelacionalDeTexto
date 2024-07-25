065c5d31243739a0ee02762c2696cc6a4a3c2f63
BragaSoterasVinicius <viniciussoteras@gmail.com>
2024-07-24 21:54:29 -0300

    new branch

--- dict.py
+++ dict.py
@@ -21,8 +21,40 @@ def palavras_unicas_por_mensagem(mensagemArray):
             palavrasUnicasPorMensagem.append(x)
     
     return palavrasUnicasPorMensagem
-
-        
+'''
+def loop_palavras_ordenadas_por_menor_index(uniquePalavras, dic, menorPalavra, novaOrdem):
+   
+    print("problema sis")
+    for n in uniquePalavras:
+        print("problema loop")
+        print(n)
+        print(menorPalavra)
+        if dic_index(dic, n) < menorPalavra:
+            menorPalavra = dic_index(dic, n)
+    novaOrdem.append(dic[menorPalavra])
+    
+    if len(uniquePalavras) == 1:
+        novaOrdem.append(uniquePalavras[0])
+        print("nova ordem")
+        print(novaOrdem)
+        return novaOrdem
+    else:
+        print("probleminha")
+        print(uniquePalavras)
+        print("probleminha")
+        print(menorPalavra)
+        uniquePalavras.remove(uniquePalavras[menorPalavra])
+        print("probleminha remove")
+        print(uniquePalavras)
+        return loop_palavras_ordenadas_por_menor_index(uniquePalavras, dic, len(uniquePalavras)-1, novaOrdem)
+    
+def palavras_ordenadas_por_menor_index(uniquePalavras, dic):
+    menorPalavra = "placeholder"
+    novaOrdem = []
+    uniquePalavras = loop_palavras_ordenadas_por_menor_index(uniquePalavras, dic, len(uniquePalavras)-1, novaOrdem)
+    print(uniquePalavras)
+    return uniquePalavras
+    '''
 #coluna
 class coluna:
     def __init__(self, titulo):
@@ -67,16 +99,23 @@ def update_value_frequencia_coluna(matrix, dic, mensagemArray, palavra):
     #mesma. Não se vão querer mudar no futuro, mas melhor anotar.
 
     #O valor original de mensagemArray deve ser o msm toda vez q o metodo roda.
+    print(mensagemArray)
     retirar_valor_de_array(palavra, mensagemArray)
     print(mensagemArray)
     
     coluna = matrix[dic_index(dic, palavra)]
     for n in mensagemArray:
         print(n)
+        print(palavra)
         #agora vamos adicionar na coluna um valor mais um para cada aparição de uma palavra
         #assume-se que todas as palavras do mensagemArray já estejam registradas como uma linha
         #no dicionário
+        print("papo reto")
+        print(dic_index(dic, n))
+        print(dic_index(dic, palavra)-1)
+        print(len(dic))
         valor = coluna.getCorpo(dic_index(dic, n) - dic_index(dic, palavra)-1)
+        
         #valor = coluna.corpo[dic_index(dic, n) - dic_index(dic, palavra)-1]
         print(dic_index(dic, n))
         print(dic_index(dic, palavra))
@@ -108,14 +147,28 @@ def build_new_palavra(value, matrix, dic):
 #puxar dicionário junto
 import pickle as p
 def saveMatrix(matrix):
+    print("saveMatrix")
     with open('matrix.pkl', 'wb') as outp:
         p.dump(matrix, outp, p.HIGHEST_PROTOCOL)
         
+def saveDic(dic):
+    print("saveDic")
+    with open('dic.pkl', 'wb') as outp:
+        p.dump(dic, outp, p.HIGHEST_PROTOCOL)
+        
 def loadMatrix():
+    print("loadMatrix")
     with open('matrix.pkl', 'rb') as m:
         matrix = p.load(m)
         return matrix
 
+def loadDic():
+    print("loadDic")
+    with open('dic.pkl', 'rb') as m:
+        dic = p.load(m)
+        return dic
+    
+#setup de testes
 def setupC():
     #método pra agilizar testes
     matrix = []
@@ -152,35 +205,71 @@ def setup(matrix, dic, n):
     if verify_coluna_existe(matrix, n) == False:
         print("nova coluna sendo adicionada! " + n)
         add_coluna(matrix, n)
-
+#response do bot
+def bot_response(inp):
+    response = "Resposta\n"
+    return response
+    
 #interface de usuário
 def user_message_to_matrix(message, matrix, dic):
     messageArray = split_mensagem(message)
     uniquePalavras = palavras_unicas_por_mensagem(messageArray)
     for m in uniquePalavras:
         setup(matrix, dic, m)
+    print(uniquePalavras)
+    print("bora bill")
     for n in uniquePalavras:
         print("HORA DO "+ n)
         update_value_frequencia_coluna(matrix, dic, messageArray, n)
+    print("matrix...")
+    for n in range(len(matrix)):
+        print(matrix[n].titulo+"..."+str(matrix[n].corpo))
+    print("dic...")
+    for n in range(len(dic)):
+        print(dic[n])
+    saveDic(dic)
+    saveMatrix(matrix)
+    bot_response(message)
+    inpt(matrix, dic)
     
+def inpt(matrix, dic):
+    message = str(input("\n"))
+    if message == "quit":
+        exit()
+    user_message_to_matrix(message, matrix, dic)
+
+def tela_inicial(matrix, dic):
+    if len(matrix)<1:
+        print("...Matrix: Sem titulo...\nVersão 1 da interface do Dict - \nGuia de\nNavegação,\nOrientação e\nSuporte \nExtrapessoal \n\n Sujeito a alterações.")
+    else:
+        print("...Matrix: "+matrix[0].titulo+"...\nVersão 1 da interface do Dict - \nGuia de\nNavegação,\nOrientação e\nSuporte \nExtrapessoal \n\n Sujeito a alterações.")
+    inpt(matrix, dic)
+    
+def new_matrix_dic(NewName):
+    #adicionar código para nomear sua própria matrix
+    matrix = []
+    dic = []
+    saveMatrix(matrix)
+    saveDic(dic)
+    tela_inicial(matrix, dic)
+        
+def old_matrix_dic():
+    try:
+        matrix = loadMatrix()
+        dic = loadDic()
+    except:
+        print("erro no carregamento da matrix. - old_matrix_dic()")
+    tela_inicial(matrix, dic)
+        
 if __name__ == "__main__":
     ch = int(input('''Aperte....\n > 1 para carregar matrix. \n > 2 para criar nova matrix do zero.\n '''))
     #adicionar código para selecionar matrix disponíveis na pasta
     if ch == 1:
-        try:
-            matrix = loadMatrix()
-            user_message_to_matrix(input("...Matrix: "+matrix[0].titulo+"\nVersão 1 da interface do Dict - \nGuia de\nNavegação,\nOrientação e\nSuporte \nExtrapessoal \n\n Sujeito a alterações."))
-        except:
-            print("erro no carregamento da matrix.")
-            yn = str(input("Deseja iniciar uma nova matrix? (S/N)"))
-            if yn.lower() == 's':
-                matrix = []
-                dic = []
-                #adicionar código para nomear sua própria matrix
-                user_message_to_matrix(input("...Matrix: Sem titulo...\nVersão 1 da interface do Dict - \nGuia de\nNavegação,\nOrientação e\nSuporte \nExtrapessoal \n\n Sujeito a alterações."), matrix, dic)
-            else:
-                exit()
+        old_matrix_dic()
     else:
-        #adicionar código para nomear sua própria matrix
-        user_message_to_matrix(input("...Matrix: "+matrix[0].titulo+"\nVersão 1 da interface do Dict - \nGuia de\nNavegação,\nOrientação e\nSuporte \nExtrapessoal \n\n Sujeito a alterações."))
+        yn = str(input("Deseja iniciar uma nova matrix? (S/N)\n(estará apagando a antiga...)"))
+        if yn.lower() == 's':
+            new_matrix_dic(str(input('Insira um novo nome para a matrix...')))
+        else:
+            exit()
             
