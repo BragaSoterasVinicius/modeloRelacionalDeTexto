b9c230e1d3bf396ffc80c25119220ab6c6844903
BragaSoterasVinicius <viniciussoteras@gmail.com>
2024-07-25 23:41:09 -0300

    como o nome da branch indica

--- dict.py
+++ dict.py
@@ -1,17 +1,12 @@
 dic = []
-#Só serve para saber que palavras estão no sistema, não confundir com coluna
-#Que são as colunas da matrix.
-
 matrix = []
-#misc
+
 def retirar_valor_de_array(valor, array):
     for n in range(array.count(valor)):
         array.remove(valor)
 
-#receber mensagem
 def split_mensagem(mensagem):
     palavrasPorMensagem = mensagem.split()
-    print(palavrasPorMensagem)
     return palavrasPorMensagem
 
 def palavras_unicas_por_mensagem(mensagemArray):
@@ -22,39 +17,24 @@ def palavras_unicas_por_mensagem(mensagemArray):
     
     return palavrasUnicasPorMensagem
 
-#coluna
 class coluna:
     def __init__(self, titulo):
         self.id = 0
         self.titulo = titulo
         self.corpo = []
-        #o titulo vai com a palavra de referencia da coluna e o corpo com quantas vezes
-        #essa palavra titulo aparece com outra que tenha um index igual ao index da palavra
-        #titulo + o index desta palavra no corpo da coluna.]
         
     def setCorpo(self, pos, value):
         if len(self.corpo) == 0:
-            print(self.corpo)
             self.corpo.append(0)
         if len(self.corpo)-1 < pos:
-            print(self.corpo)
             for n in range(len(self.corpo)-1,pos):
-                print(self.corpo)
                 self.corpo.append(0)
-                print(self.corpo)
-                print(len(self.corpo)-1)
-            print("deveria ter ido ate", pos)
-            print(value)
             self.corpo.append(value)
         else:
-            print(self.corpo[pos])
             self.corpo[pos] = value
 
     def getCorpo(self, pos):
-        print(len(self.corpo)-1)
-        print(pos)
         if len(self.corpo)-1 < pos:
-            print("rpgasd")
             self.setCorpo(pos, 0)
         return self.corpo[pos]
         
@@ -73,49 +53,18 @@ def verify_coluna_existe(matrix, palavra):
     return False
 
 def update_value_frequencia_coluna(matrix, dic, mensagemArray, palavra):
-    #Só pra que a palavra cuja frequencia está sendo medida não crie uma relação consigo
-    #mesma. Não se vão querer mudar no futuro, mas melhor anotar.
-
-    #O valor original de mensagemArray deve ser o msm toda vez q o metodo roda.
-    print(mensagemArray)
     retirar_valor_de_array(palavra, mensagemArray)
-    print(mensagemArray)
-    
-
     coluna = matrix[dic_index(dic, palavra)]
     for n in mensagemArray:
         if dic_index(dic, n) < dic_index(dic, palavra):
-            print(palavra + "[CASO ESPECIAL] item novo em coluna antiga " + n)
             colunaN = matrix[dic_index(dic, n)]
-            print(dic_index(dic, palavra))
-            print(dic_index(dic, n))
-            print(dic_index(dic, palavra) - dic_index(dic, n)-1)
             valor = colunaN.getCorpo(dic_index(dic, palavra) - dic_index(dic, n)-1)
             colunaN.setCorpo(dic_index(dic, palavra) - dic_index(dic, n)-1, valor+1)
         else:
-            print(n)
-            print(palavra)
-            #agora vamos adicionar na coluna um valor mais um para cada aparição de uma palavra
-            #assume-se que todas as palavras do mensagemArray já estejam registradas como uma linha
-            #no dicionário
-            print(len(dic))
-            print("papo reto")
-            print(dic_index(dic, n))
-            print(dic_index(dic, palavra))
-            print(dic_index(dic, n) - dic_index(dic, palavra)-1)
-            #Se o index da palavra for maior que o index de n, o valor será negativo e o processo deverá ser feito adicionando palavra no corpo n e não o contrário
             valor = coluna.getCorpo(dic_index(dic, n) - dic_index(dic, palavra)-1)
-            
-            #valor = coluna.corpo[dic_index(dic, n) - dic_index(dic, palavra)-1]
-            print(dic_index(dic, n))
-            print(dic_index(dic, palavra))
             coluna.setCorpo(dic_index(dic, n) - dic_index(dic, palavra)-1, valor+1)
-
-    print("coluna montada")
-    print(palavra +"..."+ str(coluna.corpo))
     return coluna.corpo
 
-#dic
 def is_word_in_dic(dic, word):
     for n in dic:
         if n == word:
@@ -128,72 +77,34 @@ def add_word_to_dic(dic, word):
 def dic_index(dic, word):
     return dic.index(word)
 
-#Coisas usáveis tipo factories
 def build_new_palavra(value, matrix, dic):
     add_word_to_dic(dic, value)
     add_coluna(matrix, value)
 
-#save and load
-#puxar dicionário junto
 import pickle as p
 def saveMatrix(matrix):
-    print("saveMatrix")
     with open('matrix.pkl', 'wb') as outp:
         p.dump(matrix, outp, p.HIGHEST_PROTOCOL)
         
 def saveDic(dic):
-    print("saveDic")
     with open('dic.pkl', 'wb') as outp:
         p.dump(dic, outp, p.HIGHEST_PROTOCOL)
         
 def loadMatrix():
-    print("loadMatrix")
     with open('matrix.pkl', 'rb') as m:
         matrix = p.load(m)
         return matrix
 
 def loadDic():
-    print("loadDic")
     with open('dic.pkl', 'rb') as m:
         dic = p.load(m)
         return dic
-    
-#setup de testes
-def setupC():
-    #método pra agilizar testes
-    matrix = []
-    add_coluna(matrix, "a")
-    add_coluna(matrix, "b")
-    add_coluna(matrix, "c")
-    add_coluna(matrix, "d")
-    arindic = ["a","b","c","d"]
-    '''matrix[0].corpo = [0,0,0,0,0]
-    matrix[1].corpo = [0,0,0,0,0]
-    matrix[2].corpo = [0,0,0,0,0]
-    matrix[3].corpo = [0,0,0,0,0]'''
-    
-    #for n in matrix:
-    #    n.corpo = arrayCorpoGenerico
-    dic = arindic
-    message = split_mensagem("a a a a a a a a a b b b c c c c c d")
-    #Eh o que eu escrevi la encima porem concretizada
-    for n in palavras_unicas_por_mensagem(message):
-        print("HORA DO "+ n)
-        update_value_frequencia_coluna(matrix, dic, message, n)
-        
-    #Esse loop aqui embaixo vai ser bom para debugar dps
-    print("resultados...")
-    for n in range(len(matrix)):
-        print(matrix[n].titulo+"..."+str(matrix[n].corpo))
+
 
 def setup(matrix, dic, n):
     if is_word_in_dic(dic, n) == False:
-        print("nova palavra sendo adicionada! " + n)
         add_word_to_dic(dic, n)
-        #Talvez no futuro devesse unir essas duas condicionais em uma coisa só, afinal, se a palavra existe no dicionário
-        #Também deve existir como coluna.
     if verify_coluna_existe(matrix, n) == False:
-        print("nova coluna sendo adicionada! " + n)
         add_coluna(matrix, n)
         
 #response do bot
@@ -208,20 +119,9 @@ def user_message_to_matrix(message, matrix, dic):
     uniquePalavras = palavras_unicas_por_mensagem(messageArray)
     for m in uniquePalavras:
         setup(matrix, dic, m)
-    print(uniquePalavras)
-    print("bora bill")
     
     for n in uniquePalavras:
-        print("HORA DO "+ n)
         update_value_frequencia_coluna(matrix, dic, messageArray, n)
-
-    print("matrix...")
-    for n in range(len(matrix)):
-        print(matrix[n].titulo+"..."+str(matrix[n].corpo))
-
-    print("dic...")
-    for n in range(len(dic)):
-        print(dic[n])
 
     #saveCoisas
     saveDic(dic)
@@ -236,7 +136,6 @@ def user_message_to_matrix(message, matrix, dic):
 def inpt(matrix, dic):
     message = str(input("\n"))
     if message == "quit":
-        #saveMatrix(matrix)
         exit()
     user_message_to_matrix(message, matrix, dic)
 
@@ -248,7 +147,6 @@ def tela_inicial(matrix, dic):
     inpt(matrix, dic)
     
 def new_matrix_dic(NewName):
-    #adicionar código para nomear sua própria matrix
     matrix = []
     dic = []
     saveMatrix(matrix)
@@ -265,7 +163,6 @@ def old_matrix_dic():
         
 if __name__ == "__main__":
     ch = int(input('''Aperte....\n > 1 para carregar matrix. \n > 2 para criar nova matrix do zero.\n '''))
-    #adicionar código para selecionar matrix disponíveis na pasta
     if ch == 1:
         old_matrix_dic()
     else:
--- response.py
+++ response.py
@@ -1,12 +1,8 @@
-#coluna
 class coluna:
     def __init__(self, titulo):
         self.id = 0
         self.titulo = titulo
         self.corpo = []
-        #o titulo vai com a palavra de referencia da coluna e o corpo com quantas vezes
-        #essa palavra titulo aparece com outra que tenha um index igual ao index da palavra
-        #titulo + o index desta palavra no corpo da coluna.]
         
     def setCorpo(self, pos, value):
         if len(self.corpo) == 0:
@@ -24,17 +20,11 @@ class coluna:
         return self.corpo[pos]
 
 def init_answer(question):
-    #Futuramente a energia terá que mudar com base em um valor médio dos conhecimentos
     energia = 10
-    #função principal da classe
     questionArray = build_array(question)
     conjuntoResposta = percorrer_array(questionArray, energia)
-
-    #nesse momento conjuntoReposta já deve ter as palavras que vai usar para responder, entre aspas, a pergunta
-
     return conjuntoResposta
 
-#msm coisa que o split_mensagem do dict.py
 def build_array(question):
     palavrasPorMensagem = question.split()
     return palavrasPorMensagem
@@ -45,69 +35,37 @@ def organizar_questionArray_por_index(questionArray, dic):
     for n in questionArray:
         dicIndexes.append(dic_index(dic, n))
     dicIndexes.sort()
-    print("dicIndexes..."+str(dicIndexes))
     for d in dicIndexes:
         questionArrayIndexed.append(dic[d])
-    print("questionArrayIndexed..."+str(questionArrayIndexed))
     return questionArrayIndexed
-#percebi agora que organizar todos os items por array não vai ser tão necessário, vai ser bom fazer isso aí em duplas, pois então
-#vou poder comparar uma palavra com a próxima, não tenho certeza ainda de como vai funcionar, vou usar esse aqui msm.
 
-#segundo metodo mais importante da classe(afirmação subjetiva).  
 def percorrer_array(questionArray, energia):
-    #dada uma palavra, ela só vai ter acesso às palavras com index maior que ela
-    #então se ela quiser procurar uma palavra que se encaixe nesse caso, o caminho terá
-    #que ser feito ao contrário, por enquanto o sistema vai ser esse
-    #só imagino que isso só vai mudar caso a ordem das palavras comece a importar no futuro
     matrix = loadMatrix()
     dic = loadDic()
     questionArrayByIndex = organizar_questionArray_por_index(questionArray, dic)
-    #pegar primeira palavra, encontrar as outras duas, e tentar encontrar as próximas
-    #sem perder toda a energia. já que estou começando com o menor index, com certeza os
-    #outros dois estarão dentro dele e assim em diante.
-
-    #debug
-    
-    #fim debug
-    
     responseArrayFinal = []
     for num in range(len(questionArrayByIndex)):
         listaProibida = []
         listaResposta = []
-        print("range", len(questionArrayByIndex))
         palavraInicial = questionArrayByIndex[num]
-        print("rodada ",num ," ", palavraInicial)
         if num == len(questionArrayByIndex)-1:
             break
         palavraFinal = questionArrayByIndex[num+1]
-        print("search")
         newSearch = search(palavraInicial, palavraFinal, listaProibida, energia, listaResposta, matrix, dic)
-        print("search rodada ", num, "palavra Inicial: ", palavraInicial, "\npalavra Final: ", palavraFinal)
-        print(newSearch)
         responseArrayFinal.append(newSearch)
 
-    #responseStrFinal = " ".join(map(str, responseArrayFinal))
     responseStrFinal = search_build_string(responseArrayFinal)
-    
-    print("array original "+ str(questionArrayByIndex))
-    print("array que temos"+ str(responseArrayFinal))
-    print(search_build_string(responseArrayFinal))
+ 
     return search_build_string(responseArrayFinal)
     
-#getPeso
 def getPeso(matrix, palavraInicialIndex, palavraFinalIndex):
     peso = matrix[palavraInicialIndex].corpo[palavraFinalIndex-len(matrix)+1]
     return peso
     
 def search(inicio, fim, listaProibida, energia, listaResposta, matrix, dic):
-    #esse método é só para ir de uma palavra para outra, deve ser executada EM PARES
-    #encontre f que n esteja na lista proibida e concorde com o gasto de energia
-    # na lista[inicio]
-    print(energia)
     energiaOriginal = energia
     pos = 0
     for peso in matrix[dic_index(dic, inicio)].corpo:
-        print(pos)
         if matrix[dic_index(dic, inicio)+pos+1].titulo not in listaProibida and peso < energia:
             if energia< 3:
                 listaProibida.append(matrix[dic_index(dic, inicio)+pos+1].titulo)
@@ -142,32 +100,25 @@ def add_word_to_dic(dic, word):
 def dic_index(dic, word):
     return dic.index(word)
 
-#load e save
 import pickle as p
 def saveMatrix(matrix):
-    print("saveMatrix")
     with open('matrix.pkl', 'wb') as outp:
         p.dump(matrix, outp, p.HIGHEST_PROTOCOL)
         
 def saveDic(dic):
-    print("saveDic")
     with open('dic.pkl', 'wb') as outp:
         p.dump(dic, outp, p.HIGHEST_PROTOCOL)
         
 def loadMatrix():
-    print("loadMatrix")
     with open('matrix.pkl', 'rb') as m:
         matrix = p.load(m)
         return matrix
 
 def loadDic():
-    print("loadDic")
     with open('dic.pkl', 'rb') as m:
         dic = p.load(m)
         return dic
     
-#debug response
-
 def debug():
     init_answer("Receba um bot do Bora Bill favela")
     
